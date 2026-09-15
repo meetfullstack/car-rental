40482e87c90b41c1f6b7ff51a4b4d0adc7d2ada9
Meet Upadhyay <meetupadhyay158@gmail.com>
2026-09-15 04:19:23 -0400

    Default theme to OS preference instead of hardcoded dark

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -10,8 +10,8 @@ export default function ThemeProvider({
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
+      defaultTheme="system"
+      enableSystem={true}
       disableTransitionOnChange={false}
     >
       {children}
